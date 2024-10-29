cad2bb1a5112cadb79534ed8f2918626e9ec1b0b
webQbe <webcubesolutionslk@gmail.com>
2024-10-29 19:08:36 +0530

    Added user's email, phone, & city to User() template

--- src/components/user.js
+++ src/components/user.js
@@ -19,6 +19,11 @@ const User = async () => {
             <img src="${user.picture.large}" />
             <div class="card-body">
                 <h1>${user.name.first} ${user.name.last}</h1>
+                <ul>
+                    <li>${user.email}</li>
+                    <li>${user.phone}</li>
+                    <li>${user.location.city}</li>
+                </ul>
             </div>
         </div>
     `;  
